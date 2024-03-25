22e97a0a0171b4b7af30a9fc075aae0be53225c3
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-25 18:54:37 +0700

    feat: add investment page

---
+++ lib/ui/investment/investment_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InvestmentPage extends StatelessWidget {
+  const InvestmentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Investment Page'),
+      ),
+    );
+  }
+}
