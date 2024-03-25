59483a34e11fba4571b247f7d55ce0bd55c321d8
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 01:05:05 +0700

    refactor: create header

--- lib/ui/investment/investment_page.dart
+++ lib/ui/investment/investment_page.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:team_5_motionhack/common/theme/font.dart';
 
 class InvestmentPage extends StatelessWidget {
   const InvestmentPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Investment Page'),
+    bool isSubmitProposal = true;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 21.0, vertical: 99),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Proposal yang terakhir kamu ajukan',
+                style: regularText16,
+              ),
+              Text(
+                'Kamu belum mengajukan proposal',
+                style: regularText12.copyWith(
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
