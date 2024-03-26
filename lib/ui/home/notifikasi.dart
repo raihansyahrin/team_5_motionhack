0d6d558f6e748efba14f61f7c2fff1106e179c50
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 14:39:30 +0700

    refactor: make it clean code

--- lib/ui/home/notifikasi.dart
+++ lib/ui/home/notifikasi.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'dart:async';
-import 'package:team_5_motionhack/ui/widgets/banner_konsultasi_button.dart';
-import 'package:flutter/services.dart';
-import 'package:team_5_motionhack/ui/widgets/banner_profile.dart';
-import 'package:team_5_motionhack/ui/widgets/banner_pembayaran.dart';
-import 'package:team_5_motionhack/ui/widgets/button_Selanjutnya.dart';
-import 'package:team_5_motionhack/ui/widgets/custom_app_bar.dart';
-import 'package:team_5_motionhack/ui/widgets/header_navigator.dart';
-import 'package:team_5_motionhack/ui/widgets/notifikasi_pembayaranBerhasil.dart';
+import 'package:team_5_motionhack/ui/widgets/list_consultant_widget.dart';
 
 class Notifikasi extends StatefulWidget {
   const Notifikasi({super.key});
@@ -24,37 +16,44 @@ class _NotifikasiState extends State<Notifikasi> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 34, horizontal: 24),
+          padding: const EdgeInsets.symmetric(vertical: 34, horizontal: 24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 32),
+              const SizedBox(height: 32),
               Container(
                 height: 60,
                 width: double.infinity,
-                color: Color(0xFFF8F8FF),
+                color: const Color(0xFFF8F8FF),
                 child: Row(
                   children: [
                     IconButton(
                       onPressed: () {
                         Navigator.pop(context);
                       },
-                      icon: Icon(Icons.arrow_back_ios_new_outlined, size: 20),
+                      icon: const Icon(Icons.arrow_back_ios_new_outlined,
+                          size: 20),
                     ),
-                    SizedBox(width: 111,),
-                    Text(
+                    const SizedBox(
+                      width: 111,
+                    ),
+                    const Text(
                       'Notifikasi',
                       style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.w500,
-                          fontFamily: 'Outfit'
-                      ),
+                          fontFamily: 'Outfit'),
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 32,),
-              BannerKonsultasi(nama: 'Proposalmu perlu Perbaikan', posisi: '12 Juni 2023', pic: 'assets/icons/Bell.svg')
+              const SizedBox(
+                height: 32,
+              ),
+              ListConsultantWidget(
+                  nama: 'Proposalmu perlu Perbaikan',
+                  posisi: '12 Juni 2023',
+                  pic: 'assets/icons/Bell.svg')
             ],
           ),
         ),
