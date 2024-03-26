f7ea399c4fe0e5bc396e38e1db59539eebd19ee4
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 23:44:02 +0700

    refactor: delete unused import

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter_svg/svg.dart';
 import 'package:team_5_motionhack/common/styles/colors.dart';
 import 'package:team_5_motionhack/common/theme/font.dart';
