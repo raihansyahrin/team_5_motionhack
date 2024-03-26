fcee886bfb11158360478480ed03f25e35f87cf2
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 22:11:14 +0700

    refactor: delete unused import

--- lib/ui/widgets/video_education_widget.dart
+++ lib/ui/widgets/video_education_widget.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:team_5_motionhack/common/theme/font.dart';
 
 class VideoEducationCard extends StatelessWidget {
