8aed98749a3b992d5bade4cd6fd9d8820100590f
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 11:44:49 +0700

    refactor: change background color button

--- lib/ui/registration/registration_page.dart
+++ lib/ui/registration/registration_page.dart
@@ -46,6 +46,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
     });
   }
 
+  @override
   void initState() {
     super.initState();
     _emailForm.addListener(_updateEmailEmptyStatus);
@@ -53,6 +54,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
     _konfirmasiForm.addListener(_updateKonfirmasiEmptyStatus);
   }
 
+  @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
       value: SystemUiOverlayStyle.dark,
@@ -65,147 +67,176 @@ class _RegistrationPageState extends State<RegistrationPage> {
             shrinkWrap: true,
             slivers: [
               SliverFillRemaining(
-              hasScrollBody: false,
-              child: Padding(
-                    padding: EdgeInsets.only(
-                    bottom:  MediaQuery.of(context).viewInsets.bottom,
-                    ),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 106,
+                hasScrollBody: false,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 106,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 21,
                         ),
-                        Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 21,
-                            ),
-                            child: Form(
-                                key: _formKey,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Daftar", style: mediumText28,),
-                                    const SizedBox(height: 52,),
-                                    CustomTextField(
-                                        label: "Email",
-                                        labelStyle: regularText16,
-                                        controller: _emailForm,
-                                        isRequired: false,
-
-                                        validator: (formEmail) {
-                                          if (_isDaftarClicked && (formEmail==null || formEmail.isEmpty)) {
-                                            return 'Masukkan email anda';
-                                          }
-                                          return ValidationHelpers.validateEmail(formEmail ?? '');
-                                        },
-                                        isPasswordEmpty: isPassEmpty,
-                                    ),
-                                    const SizedBox(height: 28,),
-                                    CustomTextField(
-                                        label: "Kata Sandi",
-                                        labelStyle: regularText16,
-                                        isRequired: false,
-                                        isPassword: true,
-                                        isPasswordHide: isPassHide,
-                                        controller: _passForm,
-
-                                        validator: (passwordForm) {
-                                          if (_isDaftarClicked && (passwordForm==null || passwordForm.isEmpty)) {
-                                            return 'Masukkan kata sandi anda';
-                                          }
-                                          return ValidationHelpers.validatePassword(passwordForm ?? '');
-                                        },
-                                        onPressedIconPassword: () {
-                                          setState(() {
-                                            isPassHide = !isPassHide;
-                                          });
-                                        },
-                                        isPasswordEmpty: isPassEmpty,
-                                        isPasswordVisible: !isPassEmpty,
-                                    ),
-                                    const SizedBox(height: 28,),
-                                    CustomTextField(
-                                        label: "Konfirmasi kata sandi",
-                                        labelStyle: regularText16,
-                                        isRequired: false,
-                                        isPassword: true,
-                                        isPasswordHide: isKonfirmasiHide,
-                                        controller: _konfirmasiForm,
-                                        validator: (konfirmasiPass) {
-                                          if (_isDaftarClicked && (konfirmasiPass==null || konfirmasiPass.isEmpty)) {
-                                            return 'Masukkan kembali kata sandi anda';
-                                          }
-                                          return ValidationHelpers.validateConfirmPassword(confirmPassword: _konfirmasiForm.text, password: _passForm.text);
-                                        },
-                                      onPressedIconPassword: () {
-                                        setState(() {
-                                          isKonfirmasiHide = !isKonfirmasiHide;
-                                        });
-                                      },
-                                       isPasswordEmpty: isKonfirmasiEmpty,
-                                       isPasswordVisible: !isKonfirmasiEmpty,
-                                    ),
-                                  ],
-                                ),
-                            ),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Daftar',
+                                style: mediumText28,
+                              ),
+                              const SizedBox(
+                                height: 52,
+                              ),
+                              CustomTextField(
+                                label: 'Email',
+                                labelStyle: regularText16,
+                                controller: _emailForm,
+                                isRequired: false,
+                                validator: (formEmail) {
+                                  if (_isDaftarClicked &&
+                                      (formEmail == null ||
+                                          formEmail.isEmpty)) {
+                                    return 'Masukkan email anda';
+                                  }
+                                  return ValidationHelpers.validateEmail(
+                                      formEmail ?? '');
+                                },
+                                isPasswordEmpty: isPassEmpty,
+                              ),
+                              const SizedBox(
+                                height: 28,
+                              ),
+                              CustomTextField(
+                                label: 'Kata Sandi',
+                                labelStyle: regularText16,
+                                isRequired: false,
+                                isPassword: true,
+                                isPasswordHide: isPassHide,
+                                controller: _passForm,
+                                validator: (passwordForm) {
+                                  if (_isDaftarClicked &&
+                                      (passwordForm == null ||
+                                          passwordForm.isEmpty)) {
+                                    return 'Masukkan kata sandi anda';
+                                  }
+                                  return ValidationHelpers.validatePassword(
+                                      passwordForm ?? '');
+                                },
+                                onPressedIconPassword: () {
+                                  setState(() {
+                                    isPassHide = !isPassHide;
+                                  });
+                                },
+                                isPasswordEmpty: isPassEmpty,
+                                isPasswordVisible: !isPassEmpty,
+                              ),
+                              const SizedBox(
+                                height: 28,
+                              ),
+                              CustomTextField(
+                                label: 'Konfirmasi kata sandi',
+                                labelStyle: regularText16,
+                                isRequired: false,
+                                isPassword: true,
+                                isPasswordHide: isKonfirmasiHide,
+                                controller: _konfirmasiForm,
+                                validator: (konfirmasiPass) {
+                                  if (_isDaftarClicked &&
+                                      (konfirmasiPass == null ||
+                                          konfirmasiPass.isEmpty)) {
+                                    return 'Masukkan kembali kata sandi anda';
+                                  }
+                                  return ValidationHelpers
+                                      .validateConfirmPassword(
+                                          confirmPassword: _konfirmasiForm.text,
+                                          password: _passForm.text);
+                                },
+                                onPressedIconPassword: () {
+                                  setState(() {
+                                    isKonfirmasiHide = !isKonfirmasiHide;
+                                  });
+                                },
+                                isPasswordEmpty: isKonfirmasiEmpty,
+                                isPasswordVisible: !isKonfirmasiEmpty,
+                              ),
+                            ],
+                          ),
                         ),
-                        SizedBox(height: 98,),
-                        Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 21
+                      ),
+                      const SizedBox(
+                        height: 98,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 21),
+                        child: Column(
+                          children: [
+                            CustomElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isDaftarClicked = true;
+                                });
+                                if (_formKey.currentState!.validate()) {
+                                  Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const BottomNavBar()),
+                                      ModalRoute.withName('/'));
+                                }
+                              },
+                              text: 'Daftar',
+                              fontSize: 18,
+                              textColor: kColorScheme.background,
+                              fontWeight: FontWeight.w600,
+                              minHeight: 47,
+                              backgroundColor: isPassEmpty ||
+                                      isKonfirmasiEmpty ||
+                                      isEmailEmpty
+                                  ? const Color(0xFF00584B).withOpacity(0.5)
+                                  : const Color(0xFF00584B),
+                              borderRadius: 8,
                             ),
-                            child: Column(
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                CustomElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        _isDaftarClicked = true;
-                                      });
-                                      if (_formKey.currentState!.validate()) {
-                                         Navigator.pushAndRemoveUntil(
-                                          context,
-                                          MaterialPageRoute(
-                                             builder: (context)=>const BottomNavBar()
-                                          ),
-                                          ModalRoute.withName('/')
-                                         );
-                                       }
-                                    },
-                                    text: "Daftar",
-                                    fontSize: 18,
-                                    textColor: kColorScheme.background,
-                                    fontWeight: FontWeight.w600,
-                                    minHeight: 47,
-                                    backgroundColor: isPassEmpty || isKonfirmasiEmpty || isEmailEmpty
-                                    ? const Color(0xFF001449).withOpacity(0.5) : Color(0xFF001449),
-                                    borderRadius: 8,
+                                const Text('Sudah punya Akun?'),
+                                const SizedBox(
+                                  width: 5,
                                 ),
-                                const SizedBox(height: 24,),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                  const Text("Sudah punya Akun?"),
-                                    const SizedBox(width: 5,),
-                                    GestureDetector(
-                                      onTap: () {
-                                        Navigator.push(context, MaterialPageRoute(builder: (context)=>BottomNavBar()));
-                                      },
-                                      child: Text("Masuk", style: boldUnderlineText14,),
-                                    )
-                                 ],
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const BottomNavBar()));
+                                  },
+                                  child: Text(
+                                    'Masuk',
+                                    style: boldUnderlineText14,
+                                  ),
                                 )
                               ],
-                            ),
-                        )
-                      ],
-                    ),
-              ),
-            )
-          ]
-        ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ]),
       ),
     );
   }
 }
-
