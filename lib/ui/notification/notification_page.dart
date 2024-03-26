0a5eb9e3ca09cfe3a9bb233a7626cf23b777aec5
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 17:35:16 +0700

    feat: add notification_page

---
+++ lib/ui/notification/notification_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:team_5_motionhack/common/theme/font.dart';
+import 'package:team_5_motionhack/ui/widgets/custom_app_bar.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Notifikasi'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 21.0),
+          child: Column(
+            children: [
+              Container(
+                height: 72,
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 21,
+                ),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 0.4,
+                      color: Color(0xFFC9C9C9),
+                    ),
+                  ),
+                  color: Color(0x00f8f8ff),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: SvgPicture.asset(
+                        'assets/icons/notif_icon.svg',
+                        height: 40,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Proposalmu perlu perbaikan',
+                              overflow: TextOverflow.ellipsis,
+                              style: regularText14,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              '12 Juni 2023',
+                              style: regularText12.copyWith(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
