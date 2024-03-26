fd9e9d6c778c2098c41075479eb74595ee9e34d0
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-03-26 21:46:51 +0700

    refactor: padding top bottom to 32 inside row

--- lib/ui/widgets/video_education_widget.dart
+++ lib/ui/widgets/video_education_widget.dart
@@ -33,8 +33,8 @@ class VideoEducationCard extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(
               left: 21.0,
-              top: 21,
-              bottom: 21,
+              top: 32,
+              bottom: 32,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -61,8 +61,16 @@ class VideoEducationCard extends StatelessWidget {
                   width: 59,
                   decoration: const BoxDecoration(
                     color: Color.fromARGB(255, 155, 140, 140),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: <Color>[
+                        Color.fromARGB(255, 168, 168, 168),
+                        Color.fromARGB(255, 45, 45, 45),
+                      ],
+                    ),
                     borderRadius: BorderRadius.all(
-                      Radius.circular(3),
+                      Radius.circular(4),
                     ),
                   ),
                   child: Text(
