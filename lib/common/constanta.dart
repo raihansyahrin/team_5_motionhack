f9820d8e3dabf81ce0f80a3381951e1dae9834c4
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-05-31 21:10:12 +0700

    feat: add constant file

--- lib/common/constanta.dart
+++ lib/common/constanta.dart
@@ -1,1 +1,15 @@
 
+import 'package:dio/dio.dart';
+
+const apiUrl = "";
+Dio dio = Dio(
+  BaseOptions(
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    validateStatus: (statusCode) {
+      return statusCode! < 500; 
+    },
+  ),
+);
